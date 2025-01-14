7897858eff8f8daaa3bf13165b26f08892155437
Ze Xiu <ze.xiu@ranplanwireless.com>
2025-01-14 20:48:33 +0800

    Move `TypeBinding` class to Util folder.

--- TrackDesigner/MainFormViewModel.cs
+++ TrackDesigner/MainFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using TrackDesigner.Util;
 
 namespace TrackDesigner;
 
--- TrackDesigner/Util/TypeBinding.cs
+++ TrackDesigner/Util/TypeBinding.cs
@@ -2,7 +2,7 @@
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace TrackDesigner;
+namespace TrackDesigner.Util;
 
 /// <summary>
 /// Provides a new way to define the property name by using lambda expressions.
