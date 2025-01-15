ad061c679f551baaf2a08482be35facacaaae40b
Ze Xiu <ze.xiu@ranplanwireless.com>
2025-01-15 08:37:23 +0800

    Temporary commit for canvas rendering interaction test.

---
+++ TrackDesigner/GridCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TrackDesigner;
+
+public class GridCanvas : Canvas
+{
+    protected override void OnRender(DrawingContext dc)
+    {
+        base.OnRender(dc);
+
+        var gridPen = new Pen(Brushes.DarkGray, 2);
+
+        var width = 6;
+        var height = 6;
+
+        for (var i = 1; i < width; i++) 
+            dc.DrawLine(gridPen, new Point(i * 50, 0), new Point(i * 50, height * 50));
+
+        for (var i = 1; i < height; i++)
+            dc.DrawLine(gridPen, new Point(0, i * 50), new Point(width * 50, i * 50));
+    }
+}
--- TrackDesigner/TrackPiece.cs
+++ TrackDesigner/TrackPiece.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace TrackDesigner;
@@ -19,9 +21,42 @@ public class TrackPiece : FrameworkElement
 
         PieceRender?.RenderPiece(drawingContext);
 
-        drawingContext.DrawRectangle(null, new Pen(Brushes.DarkGray, 1), new Rect(Location, new Size(Width, Height)));
+        drawingContext.DrawRectangle(Brushes.Chartreuse, new Pen(Brushes.DarkGray, 1), new Rect(Location, new Size(Width, Height)));
+
+        if (MouseState.TryPeek(out var result))
+            drawingContext.DrawText(
+                new FormattedText(result, CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                    new Typeface("MicroSoft Yahei"), 10d, Brushes.Transparent, new NumberSubstitution(), 96d),
+                new Point(10, 10));
 
         drawingContext.Pop();
+    }
+
+    public Stack<string> MouseState = new(3);
+
+    protected override void OnMouseDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseDown(e);
+
+        MouseState.Push($"MouseDown: {e.GetPosition(this)}");
+    }
+
+    protected override void OnMouseUp(MouseButtonEventArgs e)
+    {
+        base.OnMouseUp(e);
+        MouseState.Push($"MouseUp: {e.GetPosition(this)}");
+    }
+
+    protected override void OnMouseEnter(MouseEventArgs e)
+    {
+        base.OnMouseEnter(e);
+        MouseState.Push($"MouseEnter: {e.GetPosition(this)}");
+    }
+
+    protected override void OnMouseLeave(MouseEventArgs e)
+    {
+        base.OnMouseLeave(e);
+        MouseState.Push($"MouseLeave: {e.GetPosition(this)}");
     }
 }
 
