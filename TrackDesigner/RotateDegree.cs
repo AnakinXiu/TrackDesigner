4ff25ed73ec9baccf7c6c2fad221ae404ba231f8
Ze Xiu <ze.xiu@ranplanwireless.com>
2025-02-08 00:04:29 +0800

    Bind TrackPiece to ItemsControl.

---
+++ TrackDesigner/RotateDegree.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackDesigner;
+
+public enum RotateDegree
+{
+    None,
+    _90,
+    _180,
+    _270
+}
---
+++ TrackDesigner/Util/RotationExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TrackDesigner.Util;
+
+public static class RotationExtensions
+{
+    public static double ToDegree(this RotateDegree rotateDegree)
+    {
+        return rotateDegree switch
+        {
+            RotateDegree.None => 0,
+            RotateDegree._90 => 90,
+            RotateDegree._180 => 180,
+            RotateDegree._270 => 270,
+            _ => throw new ArgumentOutOfRangeException(nameof(rotateDegree), rotateDegree, null)
+        };
+    }
+
+    public static RotateDegree ToEnum(this double degree)
+    {
+        return degree switch
+        {
+            0 => RotateDegree.None,
+            90 => RotateDegree._90,
+            180 => RotateDegree._180,
+            270 => RotateDegree._270,
+            _ => throw new ArgumentOutOfRangeException(nameof(degree), degree, null)
+        };
+    }
+}
