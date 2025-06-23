eb569a3d7bc2758c3d033f37c92bfa42b6c1dac2
Ze Xiu <ze.xiu@ranplanwireless.com>
2025-06-23 08:01:41 +0800

    Introduce a TrackDesignProject class to represent a project file.

---
+++ TrackDesigner/Model/TrackDesignProject.cs
@@ -0,0 +1,18 @@
+﻿using TrackDesigner.Model.Tracks;
+
+namespace TrackDesigner.Model;
+
+public class TrackDesignProject
+{
+    public string ProjectName { get; set; }
+
+    public string Description { get; set; }
+
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public int TrackPieceWidth { get; set; }
+
+    public IEnumerable<TrackModel> TrackPieces { get; set; }
+}
