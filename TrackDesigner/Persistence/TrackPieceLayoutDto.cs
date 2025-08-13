d1dbd326187740c9cc82f8601078187a177dd0f4
Anakin.Xiu <anakin.xiu@foxmail.com>
2025-08-13 18:03:44 +0800

    Implement `TrackPieceLayoutDto` constructor.

--- TrackDesigner/Persistence/TrackPieceLayoutDto.cs
+++ TrackDesigner/Persistence/TrackPieceLayoutDto.cs
@@ -6,13 +6,14 @@ internal class TrackPieceLayoutDto
 {
     public IEnumerable<TrackPieceDto> TrackPieces { get; private set; }
 
-    public TrackPieceLayoutDto(TrackModel[][] trackPieces)
+    public TrackPieceLayoutDto(TrackModel[, ] trackPieces)
     {
-        TrackPieces = trackPieces.SelectMany(
-            (models, i) => models.Select(
-                (model, j) => new TrackPieceDto
+        TrackPieces = Enumerable.Range(0, trackPieces.GetLength(0))
+            .SelectMany(i => Enumerable.Range(0, trackPieces.GetLength(1))
+                .Select(j => new TrackPieceDto
                 {
-                    Type = model.TrackPieceType,
+                    Type = trackPieces[i, j].TrackPieceType,
+                    Orientation= trackPieces[i, j].TrackPieceOrientation,
                     X = j,
                     Y = i
                 }));
